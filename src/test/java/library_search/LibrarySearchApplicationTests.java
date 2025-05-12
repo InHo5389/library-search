8900cd2f865166782ff715f1b161851da1cd2347
qheog <qheogus55@naver.com>
2025-05-12 18:19:02 +0900

    initial commit

---
+++ src/test/java/library_search/LibrarySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package library_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
